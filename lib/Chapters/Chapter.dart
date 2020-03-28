001746667d91756fcf8f41cecdd60714762dd9ac
obitors <ashutoshagrawal1771@gmail.com>
2020-03-28 17:15:22 +0100

    Prearticle App Beta 0.0.7

---
+++ lib/Chapters/Chapter.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Chapter {
+  String name;
+  String data;
+
+  Chapter({
+      this.name,
+      this.data,
+  });
+
+
+}
---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:prearticle/Home_page.dart';
+
+void main() => runApp(
+  PreArticle()
+  );
+
+class PreArticle extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'PreArticle Application',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor: Color(0xff054587),
+      ),
+      home: Homepage(),
+    );
+  }
+}
