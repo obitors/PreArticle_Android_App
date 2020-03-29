22b6f6b6d2d9c9fc2a2cf8870b471d25f0127e6a
obitors <ashutoshagrawal1771@gmail.com>
2020-03-29 17:05:35 +0200

    Prearticle App Beta 0.0.17

--- lib/Home_page.dart
+++ lib/Home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:prearticle/Chapters/Contents.dart';
+import 'package:prearticle/Chapters/chapterData.dart';
 import 'package:prearticle/Chapters/chapterName.dart';
 
 
@@ -12,6 +13,7 @@ class _HomepageState extends State<Homepage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color(0xff6e9bdf),
       appBar: AppBar(
         leading: IconButton(
           icon: Icon(Icons.menu),
@@ -35,7 +37,16 @@ class _HomepageState extends State<Homepage> {
       body: Column (
         children: <Widget>[
           chapter_Name(),
-        ]
+          Expanded(
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(topLeft: Radius.circular(25), topRight: Radius.circular(25)),
+              ),
+              child: chapter_data(),
+            ),
+          ),
+        ],
       ),
     );
   }
