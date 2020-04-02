d270c761c2a118d7e7e303e87a99cdd17f5b3d58
obitors <ashutoshagrawal1771@gmail.com>
2020-04-03 01:16:20 +0200

    Prearticle App Beta 0.0.36

--- lib/Chapters/HundredWays.dart
+++ lib/Chapters/HundredWays.dart
@@ -1,18 +1,20 @@
 import 'package:flutter/material.dart';
 import 'dart:async';
+import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:flutter_rounded_progress_bar/flutter_rounded_progress_bar.dart';
 import 'package:flutter_rounded_progress_bar/rounded_progress_bar_style.dart';
 
 class Hundred_ways extends StatefulWidget {
   Hundred_ways({Key key}) : super(key: key);
+  
 
   @override
   _Hundred_waysState createState() => _Hundred_waysState();
 }
 
 class _Hundred_waysState extends State<Hundred_ways> {
-  final String url = 'ashutoshagrawal.in';
+  final String url = 'https://textbin.online/QaV2QNSoA7';
   List data;
   double percen=30;
   @override
@@ -23,12 +25,20 @@ class _Hundred_waysState extends State<Hundred_ways> {
 
   Future<String> getJsonData() async {
     var response = await http.get(url);
+    print(response.body);
+    setState(() {
+      var convertDataToJson = jsonDecode(response.body);
+      data = convertDataToJson['results'];
+    });
+    return "success";
   }
+
+  
 
   Widget build(BuildContext context) {
     return Scaffold(
       body: ListView.builder(
-        itemCount: 20,
+        itemCount: data == null? 0 : data.length,
         itemBuilder: (BuildContext context, int index) {
           return Container(
               padding: EdgeInsets.only(left: 25, right: 25, bottom: 15),
@@ -62,7 +72,7 @@ class _Hundred_waysState extends State<Hundred_ways> {
                               Padding(
                                 padding: EdgeInsets.only(top: 10),
                                 child: Text(
-                                  'Get out of your Death Bed',
+                                  data[index]['name'],
                                   style: TextStyle(
                                     fontSize: 15,
                                     fontWeight: FontWeight.bold,
